402a5cb49b744b2f4a151e6de12cf28a8986c141
singularity <12184989+singularity-s0@users.noreply.github.com>
2022-02-24 10:55:55 +0800

    init: gh-pages

--- README.md
+++
@@ -1,2 +0,0 @@
-# frc6907-scouting-app
- Scouting App for FRC Team 6907
--- android/app/src/main/kotlin/com/example/scouting_6907/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.scouting_6907
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
